d3014c8553a8c8a75dad65ee3c2462ee7c74142b
maxnibler <max.nibler@gmail.com>
2022-04-05 09:15:32 -0700

    changed models

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def say_hello(request):
-    return render(request, 'index.html')
+    context = {
+        "name": "max",
+        "number": 1,
+        "text": "text",
+    }
+    return render(request, 'index.html', context)
--- post/models.py
+++ post/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.TextField()
-    author = models.TextField()
-    content = models.TextField()
+    title = models.CharField(max_length=120)
+    content = models.TextField(blank=True, null=True)
+    featured = models.BooleanField(default=False)
