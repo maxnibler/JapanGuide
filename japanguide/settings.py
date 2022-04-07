549d38a251ac5deb5c8b9712a6eda836c4622ac1
maxnibler <max.nibler@gmail.com>
2022-04-07 15:48:47 -0700

    restructure again

--- japanguide/settings.py
+++ japanguide/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rr&#j(c1d(%uj-m2&c0u@la44_$q&)4ecmvxwg+et6q(qiy$z#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jace.us-west-1.elasticbeanstalk.com', '127.0.0.1']
+ALLOWED_HOSTS = ['jace.us-west-1.elasticbeanstalk.com', '127.0.0.1', '172.31.31.189']
 
 
 # Application definition
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'japanguide.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['application/templates/'],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
