371f3f232fa05f09fd4bb8acc7d17ed0b65ce59a
maxnibler <max.nibler@gmail.com>
2022-04-07 16:07:30 -0700

    settings changes for DB

--- japanguide/settings.py
+++ japanguide/settings.py
@@ -94,9 +94,9 @@ if 'RDS_DB_NAME' in os.environ:
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
         'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOST'],
+        'HOST': os.environ['RDS_HOSTNAME'],
         'PORT':  os.environ['RDS_PORT'],
         }
     }
