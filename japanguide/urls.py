7446d75d969df0de3231eda4c175992aa1a4c445
maxnibler <max.nibler@gmail.com>
2022-04-06 18:03:45 -0700

    Formatted templates for bulma

--- japanguide/urls.py
+++ japanguide/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('post/', include('post.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
     path('destination/', include('destination.urls')),
-]
+]# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
