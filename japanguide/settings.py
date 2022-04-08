36adbaa8ad06f22f21561e82b136998073392c8f
maxnibler <max.nibler@gmail.com>
2022-04-08 10:57:51 -0700

    preparing for production

--- japanguide/settings.py
+++ japanguide/settings.py
@@ -114,10 +114,6 @@ else:
             'HOST': 'localhost',
             'PORT': '5432',
         },
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        },
     }
 
 
