709dda90afb7180701a7a5d59bad81c913bb84a3
d <d>
2022-04-08 18:45:30 +0000

    d

--- japanguide/management/commands/createsu.py
+++ japanguide/management/commands/createsu.py
@@ -6,4 +6,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "Amagiinn12")
+            User.objects.create_superuser(username="admin", password="Amagiinn12")
